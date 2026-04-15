58628a8e16aa0ddb9e92658b98bc542b65f0703a
Vimal Gautam <vimal.gautam2302@gmail.com>
2026-04-15 10:59:34 +0530

    basic changes in pro

--- src/routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const rateLimiter = require('../middleware/rateLimiter');
-
-router.get('/', rateLimiter, (req, res) => {
-  res.json({ message: "Request successful 🚀" });
-});
-
-module.exports = router;
